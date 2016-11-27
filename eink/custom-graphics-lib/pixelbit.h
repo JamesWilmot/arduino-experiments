700e735dc029710d902ae48c0e909052370bf784
James Wilmot <jameswilmot2000@gmail.com>
2016-11-27 16:25:20 +1100

    Custom graphics lib: scaffolding for experimenting with a custom graphic lib for writing to a framebuffer with one bit representing a pixel

---
+++ eink/custom-graphics-lib/pixelbit.h
@@ -0,0 +1,11 @@
+
+
+int pixelbit_init();
+
+
+// use short unsigned int
+int pixelbit_point(int x, int y);
+
+int pixelbit_circle(int x, int y, int r, int w);
+
+int pixelbit_parallelogram(int x_1, int y_1, int x_2, int y_2, int w);
